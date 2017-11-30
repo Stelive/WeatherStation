c4118edf57d55a3857c271b329504cff35903841
Stefano Pedroli <stefano.pedroli@edu.itspiemonte.it>
2017-11-30 02:06:50 +0100

    Added: Loader, Title, Settings, Accordion Link to Color Main Theme: http://www.color-hex.com/color/036f4b Link to Filter method: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_list

--- js/main.js
+++ js/main.js
@@ -8,3 +8,31 @@
  * Purpose of file:
  * Prova 1
 */
+
+// loader
+var myVar;
+function loadPage() {
+    document.getElementById("container").style.display = "none";
+    myVar = setTimeout(showPage, 1000);
+    // qua invece di un intervallo facciamo partire...
+    // ...la cosa quando ha finito di caricare
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("container").style.display = "block";
+}
+
+// accordion
+var acc = document.getElementsByClassName("accordion");
+for (var i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+}
