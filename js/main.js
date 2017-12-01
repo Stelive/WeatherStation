875d58b282530917abc8d3430e31d54155be86b1
Stefano Pedroli <stefano.pedroli@edu.itspiemonte.it>
2017-12-01 01:57:53 +0100

    Accordion Pane PERFECTLY RESPONSIVE now. Added some example information. Added an example of .svg img

--- js/main.js
+++ js/main.js
@@ -89,10 +89,10 @@ for (var i = 0; i < acc.length; i++) {
     acc[i].onclick = function(){
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
+        if (panel.style.display === "flex") {
             panel.style.display = "none";
         } else {
-            panel.style.display = "block";
+            panel.style.display = "flex";
         }
     }
 }
