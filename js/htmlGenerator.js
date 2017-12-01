f1bf71e5f520d3ecb468367d35a58e2643792a83
Alessandro Oppedisano <alessandro.oppe@gmail.com>
2017-12-01 09:24:38 +0100

    Add function

--- js/htmlGenerator.js
+++ js/htmlGenerator.js
@@ -11,6 +11,8 @@
  * @exerciseTitle: Weather Station
  */
 
+var pageNode = document.body;
+
  /**
   * Function that retrun an accordion
   * @param {String} accordionName - name of accordion
@@ -20,6 +22,21 @@
 function generateAccordion(accordionName, temperature) {
   /*<button class="accordion firstAccordion">Section 1</button>
   <div class="panel">
-    <div class="divProva"></div>
+    <div class="divProva">
+
+    </div>
   </div> */
+
+  var button = document.createElement('button');
+  var firstDiv = document.createElement('div');
+  var secondDiv = document.createElement('div');
+
+  button.className("className");
+  firstDiv.className("panel");
+  secondDiv.className("divProva");
+
+  pageNode.appendChild(button);
+  button.appendChild(firstDiv);
+  firstDiv.appendChild(secondDiv);
+
 }
--- js/main.js
+++ js/main.js
@@ -38,16 +38,6 @@ for (var i = 0; i < acc.length; i++) {
     }
   }
 }
-/**
- * @file: main.js
- * @author: Gruppo ...
- * @lesson: 17 -
- * @numberOfTheExercise: ?
- * @exerciseTitle: Weather Station
- *
- * Purpose of file:
- * Prova 1
-*/
 
 //the object for the JSON
 var weatherStation = {
@@ -55,47 +45,6 @@ var weatherStation = {
   temperature : "",
   nation: ""
 };
-
-// loader
-var myVar;
-function loadPage() {
-    document.getElementById("container").style.display = "none";
-    myVar = setTimeout(showPage, 1000);
-    // qua invece di un intervallo facciamo partire...
-    // ...la cosa quando ha finito di caricare
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("container").style.display = "block";
-}
-
-// accordion
-/*
-var acc = document.getElementsByClassName("accordion");
-for (var i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  }
-}*/
-var acc = document.getElementsByClassName("accordion");
-for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "flex") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "flex";
-        }
-    }
-}
 
 
 /** * Return a object that is the conversion of json of the site
@@ -133,6 +82,7 @@ function createweatherStation(object){
 var $buttons = $("button")
 $buttons.each(function($button){
   $($button).attr('value', object.station.city);
+
   });
 }
 
